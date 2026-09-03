28cf190abe472b409f942d03ad3335be035b87f9
Eke Victor Chibuike <ekevictor216@gmail.com>
2026-09-03 20:24:40 +0100

    Add back to top button

--- script.js
+++ script.js
@@ -25,3 +25,17 @@ searchInput.addEventListener('input', function () {
     }
   });
 });
+
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+});
+
+backToTopButton.addEventListener('click', function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
